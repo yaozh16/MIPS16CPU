4c2d8f48a627ecfe8e16608509da976f6c3a84d5
陈嘉佑 <2016080034@secoder.net>
2018-12-03 17:33:36 +0800

    png转data

---
+++ img_data/png2data.py
@@ -0,0 +1,71 @@
+import sys
+import struct
+from PIL import Image
+
+seq = sys.argv[1]
+#target = sys.argv[-1]
+print('seq = ', end='')
+print(seq)
+#print('target = ', end = '')
+#print(target)
+
+def binstr(x, length):
+    str = bin(x)[2:]
+    l = length - len(str)
+    if l > 0:
+        return '0' * l + str
+    else:
+        return str
+
+def hexstr(x, length):
+    str = hex(x)[2:]
+    l = length - len(str)
+    if l > 0:
+        return '0' * l + str
+    else:
+        return str
+
+addr = 32768;
+
+
+with open('cat.data', 'wb') as f:
+    img = seq
+    im = Image.open(img)
+    width, height = im.size
+    print(im.mode)
+
+    ratio = 0
+    change = 0
+    if (width > 320):
+        width = 320
+        change = 1
+    else:
+        if width < 16:
+            exit()
+
+    if (height > 240):
+        height = 240
+        change = 1
+    else:
+        if height < 8:
+            exit()
+
+    if change == 1:
+        new_im = im.resize((width, height), Image.ANTIALIAS)
+
+    pix = new_im.load()
+
+    print(pix[0, 0])
+    for y in range(height):
+        for x in range(width):
+            tmp = hex(addr)[2:]
+            ddr = "0"*(6-len(tmp)) + tmp;
+            addr += 1
+            R = pix[x, y][0]
+            G = pix[x, y][1]
+            B = pix[x, y][2]
+            i=R/32*64+G/32*8+B/32
+            s=hex(int(i))[2:]
+            s="0"*(4-len(s))+s
+            w_str = ddr + '=' + s + "\n"
+            f.write(str.encode(w_str))
